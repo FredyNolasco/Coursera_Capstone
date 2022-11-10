f572fb4420d56220b41660beccd643daa23dbb66
FredyNolasco <47754338+FredyNolasco@users.noreply.github.com>
2022-11-09 22:20:47 -0600

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.     Escriba algunas anotaciones para explicar que este notebook se utilizará principalmente para el proyecto final.
+
+# In[1]:
+
+
+# Este notebook se utilizará para el proyecto final de ciencia de datos
+
+
+# 2.     Importe la biblioteca de pandas como pd.
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# 3.     Importe la biblioteca Numpy como np.
+
+# In[5]:
+
+
+import numpy as np
+
+
+# 4.     Imprima la siguiente declaración: ¡Hola, Proyecto Final del Curso!
+
+# In[6]:
+
+
+print("¡Hola, Proyecto Final del Curso!")
+
+
+# In[ ]:
+
+
+
+
